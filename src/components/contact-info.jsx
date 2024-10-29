8a716216b56071e5c0fb513da484239239ee84dc
Stephens37 <97484915+Stephens37@users.noreply.github.com>
2024-10-29 18:53:14 -0500

    'Text added to div with flaws'

--- src/components/contact-info.jsx
+++ src/components/contact-info.jsx
@@ -1,9 +1,36 @@
 import './contact-info.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+
+/*
+    i will need to acces both the div and the input values
+*/
+
 
 export default function ContactInfo ({inputOrPrev}) {
-    let inputVariable = <input type='text' className='contactInput'></input>
-    let divVariable = <div className='contactDiv'>{inputVariable.value}</div>
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    function InputVariable() {
+        return (
+            <>
+                <input type='text'
+                className='contactInput'
+                value={inputValue}
+                onChange={handleInputChange}/>
+            </>
+        )
+    }
+    
+    function DivVariable() {
+        return(
+            <>
+                <div className='contactDiv'>{inputValue}</div>
+            </>
+        )
+    }
     return (
         <div class='contactInfoGrid'>
             <div className='contactInfoHeader'>Contact Info</div>
@@ -11,15 +38,15 @@ export default function ContactInfo ({inputOrPrev}) {
                 <div className='basicInfo'>
                     <div className='contactContainer'>
                         <div className='contactTitle nameTitle'>Name</div>
-                         {inputOrPrev === 'preview' ? divVariable : inputVariable}
+                         {inputOrPrev === 'preview' ? <DivVariable></DivVariable>  : <InputVariable></InputVariable>}
                     </div>
                     <div className='contactContainer'>
                         <div className='contactTitle emailTitle'>Email</div>
-                        {inputOrPrev === 'preview' ? divVariable : inputVariable}
+                        {inputOrPrev === 'preview' ? <DivVariable></DivVariable> : <InputVariable></InputVariable>}
                     </div>
                     <div className='contactContainer'>
                         <div className='contactTitle numberTitle'>Number</div>
-                        {inputOrPrev === 'preview' ? divVariable : inputVariable}
+                        {inputOrPrev === 'preview' ? <DivVariable></DivVariable> : <InputVariable></InputVariable>}
                     </div>
                 </div>
             </div>

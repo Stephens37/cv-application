992a1875eee0e2bb567431443b46beb1c44e5f12
Stephens37 <97484915+Stephens37@users.noreply.github.com>
2024-10-29 19:01:53 -0500

    'Fix freezing of input after one letter typed'

--- src/components/contact-info.jsx
+++ src/components/contact-info.jsx
@@ -2,7 +2,7 @@ import './contact-info.css'
 import { useState, useRef } from 'react'
 
 /*
-    i will need to acces both the div and the input values
+    how do I separate the changing of each div value
 */
 
 
@@ -13,24 +13,6 @@ export default function ContactInfo ({inputOrPrev}) {
         setInputValue(event.target.value)
     }
 
-    function InputVariable() {
-        return (
-            <>
-                <input type='text'
-                className='contactInput'
-                value={inputValue}
-                onChange={handleInputChange}/>
-            </>
-        )
-    }
-    
-    function DivVariable() {
-        return(
-            <>
-                <div className='contactDiv'>{inputValue}</div>
-            </>
-        )
-    }
     return (
         <div class='contactInfoGrid'>
             <div className='contactInfoHeader'>Contact Info</div>
@@ -38,15 +20,24 @@ export default function ContactInfo ({inputOrPrev}) {
                 <div className='basicInfo'>
                     <div className='contactContainer'>
                         <div className='contactTitle nameTitle'>Name</div>
-                         {inputOrPrev === 'preview' ? <DivVariable></DivVariable>  : <InputVariable></InputVariable>}
+                         {inputOrPrev === 'preview' ? <div className='contactDiv'>{inputValue}</div>  : <input type='text'
+                className='contactInput'
+                value={inputValue}
+                onChange={handleInputChange}/>}
                     </div>
                     <div className='contactContainer'>
                         <div className='contactTitle emailTitle'>Email</div>
-                        {inputOrPrev === 'preview' ? <DivVariable></DivVariable> : <InputVariable></InputVariable>}
+                        {inputOrPrev === 'preview' ? <div className='contactDiv'>{inputValue}</div> : <input type='text'
+                className='contactInput'
+                value={inputValue}
+                onChange={handleInputChange}/>}
                     </div>
                     <div className='contactContainer'>
                         <div className='contactTitle numberTitle'>Number</div>
-                        {inputOrPrev === 'preview' ? <DivVariable></DivVariable> : <InputVariable></InputVariable>}
+                        {inputOrPrev === 'preview' ? <div className='contactDiv'>{inputValue}</div> : <input type='text'
+                className='contactInput'
+                value={inputValue}
+                onChange={handleInputChange}/>}
                     </div>
                 </div>
             </div>
